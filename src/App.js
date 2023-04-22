296a8ef91b85a641ec4d998b8e8c853e459f29da
Abhishek Balram <bshek@Abhisheks-Air.hub>
2023-04-22 20:20:59 +1200

    add interactivity using states

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 //importing react stuff
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { useState } from 'react';
 
-function Square({ value }) {
-  return (<button className="square">{value}</button>);
+function Square() {
+  //Implementing states: this defines 'value' as a variable that stores a state and 'setValue' as a function that will be used to update the state. 
+  const [value, setValue] = useState(null);    
+
+  //Function for handling clicks 
+  function handleClick(){
+    setValue("X")  
+  }
+
+  //return JSX element
+  return (
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return( 
   <div>
     <div className="board-row">
-      <Square value="1" />
-      <Square value="2" />
-      <Square value="3" />
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-      <Square value="4" />
-      <Square value="5" />
-      <Square value="6" />
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-      <Square value="7" />
-      <Square value="8" />
-      <Square value="9" />
+      <Square />
+      <Square />
+      <Square />
     </div>
   </div>
   );
